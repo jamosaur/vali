3774ddc5d6e6c2865b0187bba96c7e0cdc0c5188
James Wallen-Jones <j.wallen.jones@googlemail.com>
2021-03-20 00:01:46 +0000

    add HTTPS support. fixes #1

--- src/Commands/Install.php
+++ src/Commands/Install.php
@@ -27,6 +27,7 @@ class Install extends Command
             $services = $this->requestServicesMenu();
         }
 
+        $this->buildNginxConfig($services);
         $this->buildDockerComposeFile($services);
         $this->replaceEnv($services);
 
@@ -45,7 +46,11 @@ class Install extends Command
                 return $collection->prepend('        depends_on:');
             })->implode("\n");
 
-        $stubs = rtrim(collect($services)->map(function ($service) {
+        $stubs = rtrim(collect($services)
+            ->filter(function ($service) {
+                return $service !== 'https';
+            })
+            ->map(function ($service) {
             return file_get_contents(__DIR__ . '/../../stubs/' . $service . '.stub');
         })->implode(''));
 
@@ -58,10 +63,14 @@ class Install extends Command
                 return $collection->prepend('volumes:');
             })->implode("\n");
 
+        $httpsVolume = collect($services)->contains('https') ?
+            '            - \'./certificates:/etc/nginx/certificates\'' :
+            '';
+
         $composeYaml = file_get_contents(__DIR__ . '/../../stubs/docker-compose.stub');
         $composeYaml = str_replace(
-            ['{{depends}}', '{{services}}', '{{volumes}}'],
-            [empty($dependencies) ? '' : $dependencies, $stubs, $volumes],
+            ['{{depends}}', '{{services}}', '{{volumes}}', '{{httpsvolume}}'],
+            [empty($dependencies) ? '' : $dependencies, $stubs, $volumes, $httpsVolume],
             $composeYaml
         );
 
@@ -78,6 +87,7 @@ class Install extends Command
                 'mysql',
                 'redis',
                 'mailhog',
+                'https'
             ],
             0,
             null,
@@ -95,5 +105,34 @@ class Install extends Command
         );
 
         file_put_contents($this->laravel->basePath('.env'), $env);
+    }
+
+    protected function buildNginxConfig(array $services): void
+    {
+        $nginxFilePath = $this->laravel->basePath('config');
+        $file = 'http.stub';
+        if (collect($services)->contains('https')) {
+            $filesExist = true;
+            $basePath =  $this->laravel->basePath('certificates');
+            $filesToCheck = ['server.key', 'server.crt'];
+
+            foreach ($filesToCheck as $file) {
+                if (!file_exists($basePath . '/' . $file)) {
+                    $this->error('Missing ' . $file);
+                    //$filesExist = false;
+                }
+            }
+            if (!$filesExist) {
+                $this->info('Please copy these files to ' . $basePath);
+                exit;
+            }
+
+            $file = 'https.stub';
+        }
+
+        file_put_contents(
+            $nginxFilePath . '/' . 'nginx-config.conf',
+            file_get_contents(__DIR__ . '/../../stubs/nginx/' . $file)
+        );
     }
 }
